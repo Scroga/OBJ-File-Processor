b8e3ea698036cdcab430b955ab97c6a8683861ac
Ilia Riabko <ilia@Pechka02.>
2025-07-07 19:00:36 +0200

    Implement MeshData class

--- OBJProcessorProject/OBJProcessor/MeshData.cs
+++ OBJProcessorProject/OBJProcessor/MeshData.cs
@@ -1,5 +1,40 @@
-﻿namespace OBJProcessor;
+﻿using System.Numerics;
 
-class MeshData {
-    // TODO:
+namespace OBJProcessor;
+
+public record class MeshVertex
+{
+    public Vector4 Position { get; set; }
+    public List<Face> Faces { get; } = new();
+
+    public MeshVertex(Vector4 position) {
+        Position = position;
+    }
+    public MeshVertex(Vector3 position) {
+        Position = new(position, 1.0f);
+    }
+}
+
+public record class VertexData {
+    public int VertexIndex { get; }
+    public int NormalIndex { get; }
+    public int UVIndex { get; }
+
+    public VertexData(int vertex, int normal, int uv) {
+        VertexIndex = vertex;
+        NormalIndex = normal;
+        UVIndex = uv;
+    }
+}
+
+public record class Face {
+    public List<VertexData> Vertices { get; } = new();
+}
+
+
+public record class MeshData {
+    public List<MeshVertex> Vertices { get; } = new();
+    public List<Vector3> Normals { get; } = new();
+    public List<Vector2> UVs { get; } = new();
+    public List<Face> Faces { get; } = new();
 }
