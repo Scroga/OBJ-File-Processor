8b7085cf58fa62ccafa7e607b9edd9227866617f
Ilia <ilia_riabko@mail.ru>
2025-08-19 21:15:56 +1000

    Add MeshData data structures with tests

--- OBJProcessorProject/OBJProcessor/MeshData.cs
+++ OBJProcessorProject/OBJProcessor/MeshData.cs
@@ -1,27 +1,55 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Numerics;
 
 namespace OBJProcessor;
 
-public record class MeshVertex
+public record class MeshVertex // TODO: make it thread safe
 {
-    public Vector4 Position { get; set; }
-    public List<Face> Faces { get; } = new();
+    private object _lock = new();
+    private Vector4 _position;
+    private ThreadSafeList<Face> _faces;
+    
 
+    /// <summary>
+    /// 
+    ///     TODO: Lock vertices that are incident with faces?
+    /// 
+    /// </summary>
+
+    public Vector4 Position
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _position;
+            }
+        }
+        set
+        {
+            lock (_lock)
+            {
+                _position = value;
+            }
+        }
+
+    }
+    public ThreadSafeList<Face> Faces => _faces;
+
+    public MeshVertex(Vector3 position) : this(new Vector4(position, 1.0f)) { }
     public MeshVertex(Vector4 position)
     {
-        Position = position;
-    }
-    public MeshVertex(Vector3 position)
-    {
-        Position = new(position, 1.0f);
+        _position = position;
+        _faces = new();
     }
 }
 
 public record class VertexData
 {
-    public int VertexIndex { get; }
-    public int UVIndex { get; }
-    public int NormalIndex { get; }
+    public int VertexIndex { get; init; }
+    public int UVIndex { get; init; }
+    public int NormalIndex { get; init; }
 
     public VertexData(int vertex, int uv, int normal)
     {
@@ -31,15 +59,47 @@ public record class VertexData
     }
 }
 
-public record class Face
+public record class Face // TODO: Make it thread safe
 {
-    public List<VertexData> Vertices { get; } = new();
+    /// <summary>
+    /// 
+    ///     TODO: Lock vertices that are incident with faces?
+    /// 
+    /// </summary>
+    public ThreadSafeList<VertexData> Vertices { get; } = new();
+}
+
+public record class PtrWrapper<T> where T : struct
+{
+    private object _lock = new();
+    private T _data;
+    public T Data
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _data;
+            }
+        }
+        set
+        {
+            lock (_lock)
+            {
+                _data = value;
+            }
+        }
+    }
+    public PtrWrapper(T data)
+    {
+        _data = data;
+    }
 }
 
 public record class MeshData
 {
-    public List<MeshVertex> Vertices { get; } = new();
-    public List<Vector3> Normals { get; } = new();
-    public List<Vector2> UVs { get; } = new();
-    public List<Face> Faces { get; } = new();
+    public ThreadSafeList<MeshVertex> Vertices = new();
+    public ThreadSafeList<Face> Faces = new();
+    public ThreadSafeList<PtrWrapper<Vector3>> Normals = new();
+    public ThreadSafeList<PtrWrapper<Vector2>> UVCoords = new();
 }
