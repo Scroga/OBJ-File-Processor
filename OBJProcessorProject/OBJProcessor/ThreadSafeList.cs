983e1fd786f8be5781386690f86491ca9136c581
Ilia <ilia_riabko@mail.ru>
2025-08-19 21:16:37 +1000

    Add ThreadSafeList data structure with tests

---
+++ OBJProcessorProject/OBJProcessor/ThreadSafeList.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OBJProcessor;
+
+public class ThreadSafeEnumerator<T> : IEnumerator<T>
+{
+    private readonly IEnumerator<T> _inner;
+    private readonly object _lock;
+
+    public ThreadSafeEnumerator(IEnumerator<T> inner, object @lock)
+    {
+        _inner = inner;
+        _lock = @lock;
+        Monitor.Enter(_lock);
+    }
+
+    public T Current => _inner.Current;
+    object System.Collections.IEnumerator.Current => Current!;
+
+
+    public bool MoveNext()
+    {
+        return _inner.MoveNext();
+    }
+
+    public void Reset()
+    {
+        _inner.Reset();
+    }
+
+    public void Dispose()
+    {
+        Monitor.Exit(_lock);
+    }
+}
+
+public class ThreadSafeList<T> : IList<T>
+{
+    private readonly List<T> _inner;
+    private readonly object _lock = new();
+
+    public ThreadSafeList()
+    {
+        _inner = new();
+    }
+
+    public int Count
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _inner.Count;
+            }
+        }
+    }
+    public bool IsReadOnly
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _inner[index];
+            }
+        }
+        set
+        {
+            lock (_lock)
+            {
+                _inner[index] = value;
+            }
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        lock (_lock)
+        {
+            return new ThreadSafeEnumerator<T>(_inner.GetEnumerator(), _lock);
+        }
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        lock (_lock)
+        {
+            return new ThreadSafeEnumerator<T>(_inner.GetEnumerator(), _lock);
+        }
+    }
+
+    public void Add(T item)
+    {
+        lock (_lock)
+        {
+            _inner.Add(item);
+        }
+    }
+
+    public void Clear()
+    {
+        lock (_lock)
+        {
+            _inner.Clear();
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        lock (_lock)
+        {
+            return _inner.Contains(item);
+        }
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        lock (_lock)
+        {
+            _inner.CopyTo(array, arrayIndex);
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        lock (_lock)
+        {
+            return _inner.Remove(item);
+        }
+    }
+
+    public int IndexOf(T item)
+    {
+        lock (_lock)
+        {
+            return _inner.IndexOf(item);
+        }
+    }
+
+    public void Insert(int index, T item)
+    {
+        lock (_lock)
+        {
+            _inner.Insert(index, item);
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        lock (_lock)
+        {
+            _inner.RemoveAt(index);
+        }
+    }
+
+    public ReadOnlyCollection<T> AsReadOnly()
+    {
+        lock (_lock)
+        {
+            return _inner.AsReadOnly();
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        lock (_lock)
+        {
+            _inner.ForEach(action);
+        }
+    }
+
+    public bool Exists(Predicate<T> match)
+    {
+        lock (_lock)
+        {
+            return _inner.Exists(match);
+        }
+    }
+}
---
+++ OBJProcessorProject/OBJProcessorTests/ThreadSafeListTests.cs
@@ -0,0 +1,114 @@
+﻿using OBJProcessor;
+using System.Collections.Concurrent;
+
+namespace OBJProcessorTests;
+
+public class ThreadSafeListTests
+{
+    static void RunTest(int numberOfTasks, ThreadSafeList<int> list, Action<ThreadSafeList<int>> action)
+    {
+        var tasks = new Task[numberOfTasks];
+
+        for (int i = 0; i < numberOfTasks; i++)
+        {
+            tasks[i] = Task.Run(() =>
+            {
+                action(list);
+            });
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    [Fact]
+    public void SimpleAddTest()
+    {
+        int numberOfTasks = 100;
+        int numberOfIterations = 1000;
+
+        var list = new ThreadSafeList<int>();
+        RunTest(numberOfTasks, list, (ThreadSafeList<int> list) =>
+        {
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                list.Add(i);
+            }
+        });
+
+        Assert.Equal(numberOfTasks * numberOfIterations, list.Count);
+    }
+}
+public class ThreadSafeEnumeratorTests
+{
+    [Fact]
+    public void ReturnsElementsInCorrectOrder()
+    {
+        var list = new ThreadSafeList<int> { 1, 2, 3 };
+        var enumerator = list.GetEnumerator();
+
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(1, enumerator.Current);
+
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(2, enumerator.Current);
+
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(3, enumerator.Current);
+
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Fact]
+    public void EmptyCollection()
+    {
+        var emptyList = new ThreadSafeList<int> {};
+        var enumerator = emptyList.GetEnumerator();
+
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Fact]
+    public void Reset_StartsFromBeginning()
+    {
+        var list = new ThreadSafeList<int> { 1, 2 };
+        var enumerator = list.GetEnumerator();
+
+        enumerator.MoveNext();
+        enumerator.Reset();
+
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(1, enumerator.Current);
+    }
+
+    [Fact]
+    public void MultipleIterations()
+    {
+        var threadSafeList = new ThreadSafeList<int> { 1, 2, 3, 4 };
+        var results = new ConcurrentBag<int>();
+        int numberOfIterations = 300;
+
+        Parallel.For(0, numberOfIterations, _ =>
+        {
+            foreach (var item in threadSafeList)
+            {
+                results.Add(item);
+            }
+        });
+
+        Assert.Equal(threadSafeList.Count * numberOfIterations, results.Count);
+    }
+
+    [Fact]
+    public void ThrowsIfCollectionModified()
+    {
+        var list = new ThreadSafeList<int> { 1, 2, 3, 4 };
+        var enumerator = list.GetEnumerator();
+
+        enumerator.MoveNext();
+        list.Add(4);
+
+        Assert.Throws<InvalidOperationException>(() => enumerator.MoveNext());
+    }
+}
+
+// TODO: More tests
