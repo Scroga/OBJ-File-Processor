2cdab756f55143a7502d1659a5ed8bc20378a1f0
Ilia Riabko <ilia@Pechka02.>
2025-09-21 23:45:16 +0200

    Python script to run blender with .obj file

---
+++ run_blender_with_obj.py
@@ -0,0 +1,42 @@
+import subprocess
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print("Usage: python run_blender_with_obj.py path/to/blender.exe path/to/model.obj")
+    sys.exit(1)
+
+BLENDER_PATH = sys.argv[1]
+OBJ_FILE = sys.argv[2]
+
+if not os.path.isfile(BLENDER_PATH):
+    raise FileNotFoundError(f"Blender not found: {BLENDER_PATH}")
+if not os.path.isfile(OBJ_FILE):
+    raise FileNotFoundError(f"OBJ not found: {OBJ_FILE}")
+
+IMPORT_SCRIPT = "import_obj.py"
+
+
+# why i cant directly write this 
+
+script = f"""
+import bpy
+import addon_utils
+
+addon_utils.enable("io_scene_obj", default_set=True, persistent=True)
+
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete(use_global=False)
+
+obj_path = r"{OBJ_FILE}"
+
+if bpy.app.version >= (4, 0, 0):
+    bpy.ops.wm.obj_import(filepath=obj_path)
+else:
+    bpy.ops.import_scene.obj(filepath=obj_path)
+"""
+
+with open(IMPORT_SCRIPT, "w") as f:
+    f.write(script)
+
+subprocess.run([BLENDER_PATH, "--python", IMPORT_SCRIPT])
