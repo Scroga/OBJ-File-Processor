fb2e7c69342727da0627889852243ebee94de8a6
Ilia Riabko <ilia@Pechka02.>
2025-09-22 17:05:57 +0200

    Add more mesh operations

---
+++ OBJProcessorProject/OBJProcessor/MeshOperations/MeshNormalization.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Numerics;
+
+namespace OBJProcessor.MeshOperations;
+
+public static class MeshNormalizationExtention
+{
+    public static Vector3 GetMinVector(MeshData mesh)
+    {
+        var min = new Vector3(float.MaxValue);
+        foreach (var vertex in mesh.Vertices)
+        {
+            if (vertex is null) continue;
+            var vec = vertex.GetAsVec3();
+            min.X = Math.Min(min.X, vec.X);
+            min.Y = Math.Min(min.Y, vec.Y);
+            min.Z = Math.Min(min.Z, vec.Z);
+        }
+
+        return min;
+    }
+    public static Vector3 GetMaxVector(MeshData mesh)
+    {
+        var max = new Vector3(float.MinValue);
+        foreach (var vertex in mesh.Vertices)
+        {
+            if (vertex is null) continue;
+            var vec = vertex.GetAsVec3();
+            max.X = Math.Max(max.X, vec.X);
+            max.Y = Math.Max(max.Y, vec.Y);
+            max.Z = Math.Max(max.Z, vec.Z);
+        }
+
+        return max;
+    }
+
+    public static void NormalizeMesh(MeshData mesh)
+    {
+        var minTask = Task.Run(() => GetMinVector(mesh));
+        var maxTask = Task.Run(() => GetMaxVector(mesh));
+        Task.WaitAll(minTask, maxTask);
+
+        Vector3 min = minTask.Result;
+        Vector3 max = maxTask.Result;
+
+        var size = max - min;
+        float longestSide = Math.Max(Math.Max(size.X, size.Y), size.Z);
+        if(longestSide <= 0.0f) longestSide = 1.0f;
+        float scaling = 1.0f / longestSide;
+        Matrix4x4 scalingMatrix = Matrix4x4.CreateScale(scaling);
+
+        Matrix4x4 transformationMatrix = MeshTransformation.CreateTransformationMatrix(
+            scaling: new Vector3(scaling));
+
+        mesh.ApplyTransformation(transformationMatrix);
+    }
+
+    public static T Normalize<T>(this T mesh) where T : MeshData
+    {
+        NormalizeMesh(mesh);
+        return mesh;
+    }
+}
--- OBJProcessorProject/OBJProcessor/MeshOperations/MeshTransformation.cs
+++ OBJProcessorProject/OBJProcessor/MeshOperations/MeshTransformation.cs
@@ -4,11 +4,41 @@ namespace OBJProcessor.MeshOperations;
 
 public static class MeshTransformation
 {
+    public static Matrix4x4 CreateTransformationMatrix(
+        Vector3? translation = null,
+        Vector3? rotation = null,
+        Vector3? scaling = null)
+    {
+        var translationMatrix = Matrix4x4.Identity;
+        var rotationMatrix = Matrix4x4.Identity;
+        var scalingMatrix = Matrix4x4.Identity;
+
+        if (translation is not null)
+            translationMatrix = Matrix4x4.CreateTranslation(translation.Value);
+
+        if (rotation is not null)
+        {
+            Vector3 radians = rotation.Value * (MathF.PI / 180f);
+            rotationMatrix =
+                Matrix4x4.CreateRotationX(radians.X) *
+                Matrix4x4.CreateRotationY(radians.Y) *
+                Matrix4x4.CreateRotationZ(radians.Z);
+        }
+        if (scaling is not null)
+            scalingMatrix = Matrix4x4.CreateScale(scaling.Value);
+
+        return translationMatrix * rotationMatrix * scalingMatrix;
+    }
+
     public static T ApplyTransformation<T>(this T mesh, Matrix4x4 transformationMatrix) where T : MeshData
     {
         Parallel.ForEach(mesh.Vertices, vertex =>
         {
-            vertex!.Position = Vector4.Transform(vertex.Position, transformationMatrix);
+            if (vertex is not null)
+            {
+                var newPosition = Vector4.Transform(vertex.Position, transformationMatrix);
+                vertex!.Position = newPosition;
+            }
         });
         return mesh;
     }
